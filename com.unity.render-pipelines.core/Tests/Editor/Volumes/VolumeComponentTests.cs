3a1b162e695787d643b83c96cd8cc4095fd711a6
Alex Vázquez <alex.vazquez@unity3d.com>
2021-01-12 10:58:58 +0100

    Adding unit tests
    - Bunch of test checking changes done on additional properties of volumes components
    - Testing the Volume component editors to check the set and get of the overrides states.

---
+++ com.unity.render-pipelines.core/Tests/Editor/Volumes/VolumeComponentTests.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEditor.Rendering;
+
+namespace UnityEngine.Rendering.Tests.Volumes
+{
+    public class VolumeComponentEditorTests<TComponent>
+        where TComponent : VolumeComponent
+    {
+        protected TComponent component { get; set; }
+        protected VolumeComponentEditor editor { get; set; }
+        
+        [SetUp]
+        public void Init()
+        {
+            component = ScriptableObject.CreateInstance<TComponent>();
+            editor = (VolumeComponentEditor)Activator.CreateInstance(typeof(VolumeComponentEditor));
+            editor.Invoke("Init", component, null);
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            ScriptableObject.DestroyImmediate(component);
+        }
+
+        protected void CheckWithCurrentAdditionalProperties(List<string> additionalProperties)
+        {
+            var fields = component
+                .GetFields()
+                .Where(f => f.GetCustomAttribute<AdditionalAttribute>() != null)
+                .Select(f => f.Name)
+                .ToList();
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"{typeof(TComponent)}`");
+            stringBuilder.AppendLine($"\"{string.Join("\",\"", fields)}\"");
+            stringBuilder.AppendLine("vs");
+            stringBuilder.AppendLine($"\"{string.Join("\",\"", additionalProperties)}\"");
+
+            Assert.True(fields.Count == additionalProperties.Count, $"The size of the additional parameters is different: {stringBuilder}");
+            Assert.True(additionalProperties.All(fields.Contains), $"The additional parameters has changed {stringBuilder}");
+        }
+
+        protected void CheckOverridesChanges()
+        {
+            component.SetAllOverridesTo(false);
+            bool allOverridesState = (bool)editor.Invoke("AreAllOverridesTo", false);
+            Assert.True(allOverridesState);
+
+            component.SetAllOverridesTo(true);
+
+            // Was the change correct?
+            allOverridesState = (bool)editor.Invoke("AreAllOverridesTo", true);
+            Assert.True(allOverridesState);
+
+            // Enable the advance mode on the editor
+            component.SetField("m_AdvancedMode", true);
+
+            // Everything is false
+            component.SetAllOverridesTo(false);
+
+            // Disable the advance mode on the editor
+            component.SetField("m_AdvancedMode", false);
+
+            // Now just set to true the overrides of non additional properties
+            editor.Invoke("SetOverridesTo", true);
+
+            // Check that the non additional properties must be false
+            allOverridesState = (bool)editor.Invoke("AreAllOverridesTo", true);
+            Assert.False(allOverridesState);
+        }
+    }
+}
+
